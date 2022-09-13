2f274bc5966832b5b8ccc7e393c098bbe7a18f58
Людмила Алтухова <luda.tamb@mail.ru>
2022-09-13 11:43:30 +0300

    feat: add global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createGlobalStyle } from 'styled-components';
+
+import store from './store/store';
 import App from './App';
 
+const GlobalStyle = createGlobalStyle`
+  * {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+ul {
+  list-style: none;
+}
+
+button {
+  border: none;
+  background-color: none;
+}
+`;
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root'),
 );
